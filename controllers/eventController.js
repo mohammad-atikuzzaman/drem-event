f5f67ab80748f01a77b77ecafbb48ee5976468ac
Md Atikuzzaman <akash203037@gmail.com>
2025-05-31 20:14:56 +0600

    all of the events api created

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -11,6 +11,18 @@ const addEvent = async (req, res) => {
   }
 };
 
+// Get all events
+const getEvents = async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to fetch users", error: error.message });
+  }
+};
+
 // Book an event
 const bookEvent = async (req, res) => {
   try {
@@ -30,7 +42,37 @@ const bookEvent = async (req, res) => {
   }
 };
 
+// getTopEvents
+
+const getFeaturedEvents = async (req, res) => {
+  try {
+    const topEvents = await Event.find()
+      .sort({ createdAt: -1 }) // Newest first
+      .limit(3); // Get top 3
+    res.status(200).json(topEvents);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to fetch top events", error: err.message });
+  }
+};
+
+
+// get all categories
+const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Event.distinct('category');
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to fetch categories", error: err.message });
+  }
+};
+
+
 module.exports = {
   addEvent,
   bookEvent,
+  getEvents,
+  getFeaturedEvents,
+  getAllCategories
 };
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,12 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { addEvent, bookEvent } = require('../controllers/eventController');
-const authorizeAdmin = require('../middleware/authorizeAdmin');
+const {
+  addEvent,
+  bookEvent,
+  getEvents,
+  getAllCategories,
+  getFeaturedEvents,
+} = require("../controllers/eventController");
+const authorizeAdmin = require("../middleware/authorizeAdmin");
 
 // Add event (admin only)
-router.post('/add', addEvent);
+router.post("/add", addEvent);
+
+// get all events
+router.get("/all", getEvents);
+
+// get all events categories
+router.get("/categories", getAllCategories);
+
+// get top 3 events
+router.get("/featured", getFeaturedEvents);
 
 // Book an event (any user)
-router.post('/book/:id', bookEvent);
+router.post("/book/:id", bookEvent);
 
 module.exports = router;
