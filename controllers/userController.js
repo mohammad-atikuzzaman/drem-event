3e6f3410a3fb21e3363ef564812739aeeeaba22d
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-06-03 12:09:49 +0600

    admin security added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,4 @@
-const User = require('../models/User');
-
+const User = require("../models/User");
 
 // Get all of the users info
 const getUsers = async (req, res) => {
@@ -7,7 +6,21 @@ const getUsers = async (req, res) => {
     const users = await User.find();
     res.json(users);
   } catch (error) {
-    res.status(500).json({ message: 'Failed to fetch users', error: error.message });
+    res
+      .status(500)
+      .json({ message: "Failed to fetch users", error: error.message });
+  }
+};
+
+const getAdmin = async (req, res) => {
+  try {
+    const { emailId } = req.query
+    const admin = await User.findOne({ email: emailId });
+    res.json(admin.role);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to fetch users", error: error.message });
   }
 };
 
@@ -17,14 +30,19 @@ const updateUser = async (req, res) => {
   const { name, photo } = req.body;
 
   try {
-    const updatedUser = await User.findByIdAndUpdate(userId, { name, photo }, { new: true });
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { name, photo },
+      { new: true }
+    );
     res.json(updatedUser);
   } catch (err) {
-    res.status(500).json({ message: 'Error updating user' });
+    res.status(500).json({ message: "Error updating user" });
   }
 };
 
 module.exports = {
   updateUser,
-  getUsers
+  getUsers,
+  getAdmin
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { updateUser, getUsers } = require("../controllers/userController");
+const { updateUser, getUsers, getAdmin } = require("../controllers/userController");
 
 // You should protect this with your authentication middleware
 router.get("/all", getUsers)
+router.get("/isadmin", getAdmin)
 router.put('/update', updateUser);
 
 module.exports = router;
